abe999438b77f73b1fab89c1a4de1784d52ea7ce
Paul Mejia Mendoza <mejillas_33@hotmail.com>
2020-03-25 21:03:54 +0100

    terminada pequeña aplicacion de angular donde se muestran la mayoria de los pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre:string = 'Capitan America';
+  nombre2:string = 'pAuL mEjIa MenDozA';
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  personajes  :string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+  PI          :number   = Math.PI;
+  porcentaje  :number   = 0.235;
+  salario     :number   = 1234.5;
+  fecha       :Date     = new Date();
+  activar     :boolean  = true;
+  idioma      :string   = 'en';
+  videoUrl    :string   = 'https://www.youtube.com/embed/z5svoighsdk';
+  
+  valorPromesa = new Promise<string>( (resolve) => {
+
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  })
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: 20
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { registerLocaleData } from '@angular/common';
+import localES from '@angular/common/locales/es';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+registerLocaleData(localES);
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
